8a0ad418c6562aaf5b66e31ea4f61a3559d1de21
mtozluu <melihtozlu29@gmail.com>
2023-07-04 21:51:19 +0300

    Update savasanhud.py

--- savasan hud/savasanhud.py
+++ savasan hud/savasanhud.py
@@ -1,40 +1,44 @@
-# cv2 modulünü import etme
+# cv2 modulünü import etme.
 import cv2 
-# kamera görüntüsü almak için videocapture kullanma
+
+# Video görüntüsü almak için videocapture kullanma.
 video=cv2.VideoCapture(0)
 
+# Aktif video çözünürlüğünün bulunması ve yazdırılması.
+a=int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+b=int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(f"Çözünürlük: {a}x{b}")
+
+# Hedef vuruş alanı dikdörtgeninin başlangıç x ve y sinin belirlenmesi.
+x=int(a/2-a/4)
+y=int(b/2-4*b/10)
+
+# Hedef vuruş alanı dikdörtgeninin genişliği uzunluğunun belirlenmesi.
+gen=int(a/2)
+uz=int(b*8/10)
+
 while True:
-    # alınan görüntünün okunması
+    # Alınan görüntünün okunması.
     ret,goruntu=video.read()
     
-    # Hedef vuruş alanı dikdörtgeninin başlangıç x ve y sinin belirlenmesi 
-    x=int(goruntu.shape[1]/2-goruntu.shape[1]/4)
-    y=int(goruntu.shape[0]/2-4*goruntu.shape[0]/10)
-    
-    # Hedef vuruş alanı dikdörtgeninin genişliği uzunluğunun belirlenmesi
-    gen=int(int(goruntu.shape[1])/2)
-    uz=int(int(goruntu.shape[0])*8/10)
-    
-    # Dikdörtgenin rectangel komutu ile çıktı alınması
+    # Hedef vuruş alanı dikdörtgeninin rectangel komutu ile çıktı alınması.
     cv2.rectangle(goruntu,(x,y),( x + gen , y+uz),(204,0,102),4)
     
-    # Hedef vuruş alanı yazısı oluşturulması
-    cv2.putText(goruntu,"Av: Hedef Vurus Alani",(int(goruntu.shape[1]/4),int(goruntu.shape[0]*9/10)),1,1,(0,0,0),1)
+    # Hedef vuruş alanı yazısı oluşturulması.
+    cv2.putText(goruntu,"Av: Hedef Vurus Alani",(int(a/4),int(b*9/10)-2),1,1,(0,0,0),1)
     
-    # Kamera görüş alanı dikdörtgeni oluşturulması
-    cv2.rectangle(goruntu,(0,0),(int(goruntu.shape[1]),int(goruntu.shape[0])),(0,153,0),4)
+    # Kamera görüş alanı dikdörtgeni oluşturulması.
+    cv2.rectangle(goruntu,(0,0),(a,b),(0,153,0),4)
 
-    # Kamera görüş alanı yazısı oluşturulması
-    cv2.putText(goruntu,"Ak: Kamera Gorus Alani",(0,(int(goruntu.shape[0])-2)),1,1,(0,0,0),1)
-    
-    # Oluşturulan görüntünün gösterilmesi
+    # Kamera görüş alanı yazısı oluşturulması.
+    cv2.putText(goruntu,"Ak: Kamera Gorus Alani",(0,b-2),1,1,(0,0,0),1)
+
+    # Oluşturulan görüntünün gösterilmesi.
     cv2.imshow("kamera",goruntu)
     
-    # while Çıkış fonksiyonun oluşturulması
+    # while Çıkış fonksiyonun oluşturulması.
     if cv2.waitKey(5) & 0xFF ==ord("q"):
-        print(f"Çözünürlük: {int(goruntu.shape[1])}x{int(goruntu.shape[0])}")   
         break
-
 
 goruntu.release()
 cv2.destroyAllWindows()
